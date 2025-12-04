3ebe0a2e49ff52094e77f89703282f59cf3aa507
jesus13jjj <josuellanas13@gmail.com>
2025-12-04 02:11:10 -0600

    Update db.js

--- db.js
+++ db.js
@@ -4,13 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT || 3306),
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default pool;
+
